8997397128a7c43f9ec8ea3d42f9195016b0e1dc
Anthony Brignano <abrignano@comcast.net>
2020-12-12 19:54:25 -0500

    update content

--- card.js
+++ card.js
@@ -1,3 +1,118 @@
 #!/usr/bin/env node
 
-console.log('doing business')
+'use strict'
+
+const boxen = require("boxen");
+const chalk = require("chalk");
+const inquirer = require("inquirer");
+const clear = require("clear");
+const open = require("open");
+const fs = require('fs');
+const request = require('request');
+const path = require('path');
+const ora = require('ora');
+const cliSpinners = require('cli-spinners');
+clear();
+
+const prompt = inquirer.createPromptModule();
+
+const questions = [
+    {
+        type: "list",
+        name: "action",
+        message: "What you want to do?",
+        choices: [
+            {
+                name: `Send me an ${chalk.green.bold("Email")}.`,
+                value: () => {
+                    open("mailto:anthonybrignano@gmail.com");
+                    console.log("\nDone, see you soon at inbox.\n");
+                }
+            },
+            {
+                name: `Download my ${chalk.magentaBright.bold("Resume")}.`,
+                value: () => {
+                    // cliSpinners.dots;
+                    const loader = ora({
+                        text: ' Downloading Resume',
+                        spinner: cliSpinners.material,
+                    }).start();
+                    let pipe = request('https://drive.google.com/file/d/1IHV9I8taSda_YsBrzC0wwmGJXUnd5QF2/export/pdf').pipe(fs.createWriteStream('./anthony-resume.pdf'));
+                    pipe.on("finish", function () {
+                        let downloadPath = path.join(process.cwd(), 'anthony-resume.pdf')
+                        console.log(`\nResume Downloaded at ${downloadPath} \n`);
+                        open(downloadPath)
+                        loader.stop();
+                    });
+                }
+            },
+            {
+                name: "Just quit.",
+                value: () => {
+                    console.log("Have a good day!\n");
+                }
+            }
+        ]
+    }
+];
+
+const data = {
+    name: chalk.bold.green("             Anthony Brignano"),
+    handle: chalk.white("@brignano"),
+    work: `${chalk.white("Software Engineer at")} ${chalk
+        .hex("#3A5A78")
+        .bold("The Hartford")}`,
+    github: chalk.gray("https://github.com/") + chalk.green("brignano"),
+    linkedin: chalk.gray("https://linkedin.com/in/") + chalk.blue("brignano"),
+    twitter: chalk.gray("https://twitter.com/") + chalk.cyan("brignano_"),
+    web: chalk.cyan("https://brignano.io"),
+    npx: chalk.red("npx") + " " + chalk.white("brignano"),
+
+    labelWork: chalk.white.bold("       Work:"),
+    labelGitHub: chalk.white.bold("     GitHub:"),
+    labelLinkedIn: chalk.white.bold("   LinkedIn:"),
+    labelTwitter: chalk.white.bold("    Twitter:"),
+    labelWeb: chalk.white.bold("        Web:"),
+    labelCard: chalk.white.bold("       Card:")
+};
+
+const me = boxen(
+    [
+        `${data.name}`,
+        ``,
+        `${data.labelWork}  ${data.work}`,
+        ``,
+        `${data.labelGitHub}  ${data.github}`,
+        `${data.labelLinkedIn}  ${data.linkedin}`,
+        `${data.labelTwitter}  ${data.twitter}`,
+        `${data.labelWeb}  ${data.web}`,
+        ``,
+        `${data.labelCard}  ${data.npx}`,
+        ``,
+        `${chalk.italic("My inbox is always open. Whether you have a")}`,
+        `${chalk.italic(
+            "question or just want to say hi, I will try "
+        )}`,
+        `${chalk.italic(
+            "my best to get back to you!"
+        )}`
+    ].join("\n"),
+    {
+        margin: 1,
+        float: 'center',
+        padding: 1,
+        borderStyle: "single",
+        borderColor: "green"
+    }
+);
+
+console.log(me);
+const tip = [
+    `Tip: Try ${chalk.cyanBright.bold(
+        "cmd/ctrl + click"
+    )} on the links above`,
+    '',
+].join("\n");
+console.log(tip);
+
+prompt(questions).then(answer => answer.action());
---
+++ readme.md
@@ -0,0 +1,9 @@
+# NPX CARD
+This my NPX card unique style to connect with me directly via console or terminal
+
+👇 just hit 
+```bash
+npx brignano
+```
+
+> 100% credit for this idea goes to [anmol098/npx_card](https://github.com/anmol098/npx_card)
